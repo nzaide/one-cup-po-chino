d0a20a25a450e97e3ee68e4819ecb74d0269ce59
norozaide <norozaide@gmail.com>
2022-06-09 04:55:17 +0800

    edited a few to beautify the UI

--- One-Cup-Po-Chino/src/components/Footer.js
+++ One-Cup-Po-Chino/src/components/Footer.js
@@ -4,21 +4,23 @@ import { Box, Container, Row, Column, FooterLink, Heading} from "./FooterStyles"
 import { AiOutlineFacebook } from "react-icons/ai";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { FaTiktok } from "react-icons/fa";
-
+import { FiTwitter } from "react-icons/fi";
+import { RiYoutubeLine } from "react-icons/ri";
 
 const Footer = () => {
 return (
   <Box>
-  <h6 style={{ color: "black",
+  <h6 style={{ color: "gray",
         textAlign: "center",
-        marginTop: "80px" }}>
+        marginTop: "0px" }}>
        <p>Copyright &copy; 2022 N.P.Z. All Rights Reserved</p>
   </h6>
-  <h5 style={{ color: "black",
+  <h5 style={{ color: "gray",
         textAlign: "center",
-        marginTop: "30px" }}>
-       <AiOutlineFacebook /> <AiOutlineInstagram /> <FaTiktok />
+        marginTop: "0px" }}>
+       <AiOutlineFacebook /> <AiOutlineInstagram /> <FaTiktok />  <FiTwitter /> <RiYoutubeLine /> 
   </h5>
+
   </Box>
 );
 };
