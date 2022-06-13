b0c64e847f26f18b03e183430d8a78f3aef85595
norozaide <norozaide@gmail.com>
2022-06-13 12:05:32 +0800

    modified Products page

--- One-Cup-Po-Chino/src/components/ProductCard.js
+++ One-Cup-Po-Chino/src/components/ProductCard.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Row, Col, Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image'
 
 export default function ProductCard({productProp}) {
 
@@ -16,19 +17,35 @@ export default function ProductCard({productProp}) {
 
 	const { _id, name, description, price, image } = productProp;
 
-	return(
-				<Card className="m-2" style={{height: "38rem"}}>
-				<Card.Img className="w-100" variant="top" src={image} />
+	return(	
+		<>
+			<Card className="m-2 border-warning" style={{height: "34rem"}}>
+				<Card.Img className="img-fluid p-2" variant="top" src={image} />
 
-				<Card.Body>
-					<Card.Title className="bg-light text-dark" style={{fontWeight: 'bold'}}> { name } </Card.Title>
+				<Card.Body className="">
+					<Card.Title className="bg-warning text-dark" style={{fontWeight: 'bold'}}> { name } </Card.Title>
 
 					<Card.Subtitle>Price:</Card.Subtitle>
 					<Card.Text><span>&#8369;</span>  { price } </Card.Text>
 
-					<Button variant="warning" className="bg-dark text-warning" style={{fontWeight: 'bold'}} as={ Link } to={`/products/findproduct/${_id}`}>See More</Button>
+					<Button variant="warning" className="bg-dark text-warning" style={{fontWeight: 'bold'}} as={ Link } to={`/products/findproduct/${_id}`}>Buy Now</Button>
 				</Card.Body>
 			</Card>
+
+
+			<Card className="m-2 border-warning" style={{height: "34rem"}}>
+				<Card.Img className="img-fluid p-2" variant="top" src={image} />
+
+				<Card.Body className="">
+					<Card.Title className="bg-warning text-dark" style={{fontWeight: 'bold'}}> { name } </Card.Title>
+
+					<Card.Subtitle>Price:</Card.Subtitle>
+					<Card.Text><span>&#8369;</span>  { price } </Card.Text>
+
+					<Button variant="warning" className="bg-dark text-warning" style={{fontWeight: 'bold'}} as={ Link } to={`/products/findproduct/${_id}`}>Buy Now</Button>
+				</Card.Body>
+			</Card>
+		</>
 		)
 }
 
